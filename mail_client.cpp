1f3d73724535167adec2ce69ef0ce7b4241fc5c7
jack <zakiazad.ops@gmail.com>
2019-02-17 12:19:55 +0600

    almost done

--- mail_client.cpp
+++ mail_client.cpp
@@ -11,17 +11,75 @@
 #include <netinet/in.h>
 #include <sys/time.h> //FD_SET, FD_ISSET, FD_ZERO macros
 #include <iostream>
+#include <sstream>
+
+#define __200 "200 (nonstandard success response, see rfc876)";
+#define __211 "211 System status, or system help reply";
+#define __214 "214 Help message";
+#define __220 "220 <domain> Service ready";
+#define __221 "221 <domain> Service closing transmission channel";
+#define __250 "250 Requested mail action okay, completed";
+#define __251 "251 User not local; will forward to <forward-path>";
+#define __252 "252 Cannot VRFY user, but will accept message and attempt delivery";
+#define __354 "354 Start mail input; end with <CRLF>.<CRLF>";
+#define __421 "421 <domain> Service not available, closing transmission channel";
+#define __450 "450 Requested mail action not taken: mailbox unavailable";
+#define __200 "451 Requested action aborted: local error in processing";
+#define __452 "452 Requested action not taken: insufficient system storage";
+#define __500 "500 Syntax error, command unrecognised";
+#define __501 "501 Syntax error in parameters or arguments";
+#define __502 "502 Command not implemented";
+#define __503 "503 Bad sequence of commands";
+#define __504 "504 Command parameter not implemented";
+#define __521 "521 <domain> does not accept mail (see rfc1846)";
+#define __530 "530 Access denied (???a Sendmailism)";
+#define __550 "550 Requested action not taken: mailbox unavailable";
+#define __551 "551 User not local; please try <forward-path>";
+#define __552 "552 Requested mail action aborted: exceeded storage allocation";
+#define __553 "553 Requested action not taken: mailbox name not allowed";
+#define __554 "554 Transaction failed";
 
 using namespace std;
 
+string toUser(string s){
+    // argument was user@host:PORT
+    // extracts the user out of argument
+
+    ostringstream os;
+    for(int i=0; s[i] != '@'; i++)
+        os << s[i];
+
+    return os.str();
+}
+
+string toHost(string s){
+    // argument was user@host:PORT
+    // extracts the host out of argument
+
+    ostringstream os;
+    bool flag = false;
+    for(int i=0; s[i] != ':'; i++){
+        if(flag==true)
+            os << s[i];
+        if(s[i] == '@')
+            flag = true;
+    }
+
+    return os.str();
+}
+
 
 int main(int argc, char const *argv[]){
+
     sockaddr_in address, servAddress;
     int client, PORT;
     char buffer[1024];
-    cout << "Type the server port number: ";
-    cin >> PORT;
+    string argument(argv[1]), host, user;
+    cout << argument << endl;
 
+    cout << toUser(argument) << endl;
+    cout << toHost(argument) << endl;
+/*
     client = socket(AF_INET, SOCK_STREAM, 0);
     servAddress.sin_family = AF_INET;
     servAddress.sin_port = htons(PORT);
@@ -39,4 +97,6 @@ int main(int argc, char const *argv[]){
     // quit
 
     close(client);
+
+    */
 }
--- mail_server.cpp
+++ mail_server.cpp
@@ -1,3 +1,4 @@
+#include <fstream>
 #include <vector>
 #include <stdio.h>
 #include <string.h> //strlen
@@ -15,6 +16,32 @@
 
 #define TRUE 1
 #define FALSE 0
+
+#define __200 "200 (nonstandard success response, see rfc876)";
+#define __211 "211 System status, or system help reply";
+#define __214 "214 Help message";
+#define __220 "220 <domain> Service ready";
+#define __221 "221 <domain> Service closing transmission channel";
+#define __250 "250 Requested mail action okay, completed";
+#define __251 "251 User not local; will forward to <forward-path>";
+#define __252 "252 Cannot VRFY user, but will accept message and attempt delivery";
+#define __354 "354 Start mail input; end with <CRLF>.<CRLF>";
+#define __421 "421 <domain> Service not available, closing transmission channel";
+#define __450 "450 Requested mail action not taken: mailbox unavailable";
+#define __200 "451 Requested action aborted: local error in processing";
+#define __452 "452 Requested action not taken: insufficient system storage";
+#define __500 "500 Syntax error, command unrecognised";
+#define __501 "501 Syntax error in parameters or arguments";
+#define __502 "502 Command not implemented";
+#define __503 "503 Bad sequence of commands";
+#define __504 "504 Command parameter not implemented";
+#define __521 "521 <domain> does not accept mail (see rfc1846)";
+#define __530 "530 Access denied (???a Sendmailism)";
+#define __550 "550 Requested action not taken: mailbox unavailable";
+#define __551 "551 User not local; please try <forward-path>";
+#define __552 "552 Requested mail action aborted: exceeded storage allocation";
+#define __553 "553 Requested action not taken: mailbox name not allowed";
+#define __554 "554 Transaction failed";
 
 /*
     200 	(nonstandard success response, see rfc876)
@@ -53,9 +80,37 @@ void handle(int clientFd, int i){
     send(clientFd, s.c_str(), s.size(), 0);
 }
 
+string toUser(string s){    // not yet implemented
+    // argument was user@host:PORT
+    // extracts the user out of argument
+
+    ostringstream os;
+    for(int i=0; s[i] != '@'; i++)
+        os << s[i];
+
+    return os.str();
+}
+
+string toHost(string s){
+    // argument was user@host:PORT
+    // extracts the host out of argument
+
+    ostringstream os;
+    bool flag = false;
+    for(int i=0; s[i] != ':'; i++){
+        if(flag==true)
+            os << s[i];
+        if(s[i] == '@')
+            flag = true;
+    }
+
+    return os.str();
+}
+
 int main(){
     int socketFD, PORT, opt=1;
     char buffer[1024];
+    string input, output, user;
     sockaddr_in address;
 
     PORT = (rand()%9000)+1000;
@@ -77,15 +132,95 @@ int main(){
         int newSockFd = accept(socketFD, (sockaddr*)&address, (socklen_t*)&addrlen);
         char *welcomeMessage = "Welcome from server, connection established";
         send(newSockFd, welcomeMessage, strlen(welcomeMessage), 0);
-        // hello
+        // hello: if hello req then ok(200), or systax error(500)
         read(newSockFd, buffer, 1024);
+        input = string(buffer);
+        if(input.substr(0, 4) == "HELO"){
+            output = __200;
+            send(newSockFd, output.c_str(), output.size(), 0);
+        }
+        else{
+            output = __500;
+            send(newSockFd, output.c_str(), output.size(), 0);
+            continue;
+        }
+
         // mail_from
         read(newSockFd, buffer, 1024);
+        input = string(buffer);
+        if(input.substr(0, 9) == "MAIL FROM"){
+            output = __200;
+            send(newSockFd, output.c_str(), output.size(), 0);
+        }
+        else{
+            output = __500;
+            send(newSockFd, output.c_str(), output.size(), 0);
+            continue;
+        }
         // rcpt_to
         read(newSockFd, buffer, 1024);
+        input = string(buffer);
+        if(input.substr(0, 7) != "RCPT TO"){
+            output = __500;
+            send(newSockFd, output.c_str(), output.size(), 0);
+            continue;
+        }
+        else
+        {
+            user = toUser(input);
+            user = user + ".txt";
+            ifstream file(user.c_str());
+            if(!file.good()){   // user doesnot exixts
+                output = __450;
+                send(newSockFd, output.c_str(), output.size(), 0);
+                continue;
+            }
+            else
+            {
+                // user exists, you may proceed
+                output = __250;
+                send(newSockFd, output.c_str(), output.size(), 0);
+            }
+            
+        }
+        
         // data
         read(newSockFd, buffer, 1024);
+        input = string(buffer);
+        if(input.substr(0, 4) == "DATA"){
+            output = __354;
+            send(newSockFd, output.c_str(), output.size(), 0);
+        }
+        else{
+            output = __500;
+            send(newSockFd, output.c_str(), output.size(), 0);
+            continue;
+        }
+
+        ofstream file(user.c_str(), ios_base::app);
+        while(true){
+            read(newSockFd, buffer, 1024);
+            if(strcmp(buffer, ".") == 0)    // <.> recieved: end of message
+                break;
+            file << string(buffer);
+        }
+        file << endl;
+        file.close();
+        
+
         // quit
+        read(newSockFd, buffer, 1024);
+        input = string(buffer);
+        if(input.substr(0, 4) == "DATA"){
+            output = __354;
+            send(newSockFd, output.c_str(), output.size(), 0);
+        }
+        else{
+            output = __500;
+            send(newSockFd, output.c_str(), output.size(), 0);
+            continue;
+        }
+
     }
 
 }
